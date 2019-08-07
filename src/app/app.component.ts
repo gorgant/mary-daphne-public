4818c2d4a4290a7b1c76f88b43452c6267bce04c
Gorgant CX1 <gorgant@gorgant.com>
2019-08-07 16:02:00 +0300

    upgrade to 1.1.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ import { PublicImagePaths } from 'shared-models/routes-and-paths/image-paths.mod
 })
 export class AppComponent implements OnInit {
   title = metaTagDefaults.maryDaphnePublic.metaTagDefaultTitle;
-  appVersion = '1.1.3';
+  appVersion = '1.1.4';
 
   private userAuthenticationRequested: boolean;
   private userLoaded: boolean;
