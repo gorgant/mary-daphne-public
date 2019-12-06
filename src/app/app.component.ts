4096b7002a373603cf43ffbd536f3a32fa2d40bc
Gorgant CX1 <gorgant@gorgant.com>
2019-12-06 18:21:34 +0800

    update to 1.2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ import { DownloadPromoComponent } from './shared/components/email-collection/dow
 })
 export class AppComponent implements OnInit {
   title = metaTagDefaults.maryDaphnePublic.metaTagDefaultTitle;
-  appVersion = '1.1.9';
+  appVersion = '1.2.0';
 
   private userAuthenticationRequested: boolean;
   private userLoaded: boolean;
