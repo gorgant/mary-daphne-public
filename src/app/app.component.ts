ca5e2f8d5274ada0d7786aa36dc1106bc2610bbc
Gorgant CX1 <gorgant@gorgant.com>
2020-08-23 16:39:54 -0400

    upgrade to 1.3.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ import { PublicAppRoutes } from 'shared-models/routes-and-paths/app-routes.model
 })
 export class AppComponent implements OnInit {
   title = metaTagDefaults.maryDaphnePublic.metaTagDefaultTitle;
-  appVersion = '1.3.1';
+  appVersion = '1.3.2';
 
   private userAuthenticationRequested: boolean;
   private userLoaded: boolean;
